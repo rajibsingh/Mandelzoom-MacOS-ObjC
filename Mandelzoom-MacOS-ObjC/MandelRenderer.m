7125f4a48b5d3d2692019ea1c82579d39f19b7fb
Raj Singh <rajibsingh@gmail.com>
2021-07-09 22:41:05 -0400

    so there's a renderer, the renderer is generating a CGIMage and sending it to the Mandelview so this is progress

--- Mandelzoom-MacOS-ObjC/MandelRenderer.m
+++ Mandelzoom-MacOS-ObjC/MandelRenderer.m
@@ -20,8 +20,40 @@
 }
 
 -(NSImage*) render {
-    NSImage* imageObj = [NSImage imageNamed:@"klarion"];
-    return imageObj;
+    
+    int width = 1000;
+    int height = 1000;
+    
+    int data[1000][1000];
+
+    
+    size_t bufferLength = width * height * 4;
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, data, bufferLength, NULL);
+    size_t bitsPerComponent = 8;
+    size_t bitsPerPixel = 32;
+    size_t bytesPerRow = 4 * width;
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedLast;
+    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
+
+    CGImageRef iref = CGImageCreate(width,
+                                    height,
+                                    bitsPerComponent,
+                                    bitsPerPixel,
+                                    bytesPerRow,
+                                    colorSpaceRef,
+                                    bitmapInfo,
+                                    provider,   // data provider
+                                    NULL,       // decode
+                                    YES,        // should interpolate
+                                    renderingIntent);
+    NSImage *image = [[NSImage alloc] initWithCGImage:iref size:NSMakeSize(width, height)];
+    return image;
+//    CGImageRef cgim = CGImageCreate(width, height, 8, bitsPerPixel, size_t bytesPerRow, cgColorSpace, CGBitmapInfo bitmapInfo, CGDataProviderRef provider, const CGFloat *decode, shouldInterpolate, CGColorRenderingIntent.defaultIntent());
+//
+//
+//    NSImage* imageObj = [NSImage imageNamed:@"klarion"];
+//    return imageObj;
     
 }
 
